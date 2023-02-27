9438d95cc4b18b03882652365d3da843c542846d
StarletIT <85713082+StarletIT@users.noreply.github.com>
2023-02-27 22:49:43 +0200

    fddf

---
+++ public/Wazefni/Requirements/JS/javascript.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+
+    $('.SpecialOffersSlider').slick({
+        dots: true,
+        arrows: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 5,
+        autoplay: true,
+        infinite: true,
+        slidesToScroll: 5,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+    $('.SpecialOffersSlider .slick-dots li').html('<div></div>')
+});
+
+
+$(window).on('load',function () {
+    // setTimeout(function(){
+    //     $('.PreloaderInner').addClass('animate__fadeOutUp')
+    // }, 1000)
+    // setTimeout(function(){
+    //     $('.Preloader').remove()
+    // }, 2000)
+    $('.Preloader').remove()
+});
+
+$(window).on('resize',function () {
+    setTimeout(function (){
+        $('.SpecialOffersSlider .slick-dots li').html('<div></div>')
+    }, 500)
+});
